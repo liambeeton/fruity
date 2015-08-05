c5cb2852361627eb1e366b0dae22456d9cfde5cd
Liam Beeton <liam.beeton@gmail.com>
2015-08-05 19:49:11 +0100

    added fruit entity attributes

--- Fruity/Logic/Models/Fruit.h
+++ Fruity/Logic/Models/Fruit.h
@@ -10,4 +10,12 @@
 
 @interface Fruit : NSObject
 
+@property (nonatomic, readonly) NSDecimalNumber *price;
+@property (nonatomic, readonly) NSString *type;
+@property (nonatomic, readonly) NSUInteger weight;
+
+- (instancetype)initWithPrice:(NSDecimalNumber *)price
+                         type:(NSString *)type
+                       weight:(NSUInteger)weight;
+
 @end
--- Fruity/Logic/Models/Fruit.m
+++ Fruity/Logic/Models/Fruit.m
@@ -10,4 +10,32 @@
 
 @implementation Fruit
 
+- (instancetype)initWithPrice:(NSDecimalNumber *)price
+                         type:(NSString *)type
+                       weight:(NSUInteger)weight {
+    self = [super init];
+    if (self) {
+        _price = [price copy];
+        _type = [type copy];
+        _weight = weight;
+    }
+    return self;
+}
+
+- (BOOL)isEqual:(id)obj {
+    if(![obj isKindOfClass:[Fruit class]]) return NO;
+
+    Fruit *other = (Fruit *)obj;
+    
+    BOOL priceIsEqual = self.price == other.price || [self.price isEqual:other.price];
+    BOOL typeIsEqual = self.type == other.type || [self.type isEqual:other.type];
+    BOOL weightIsEqual = self.weight == other.weight;
+    
+    return priceIsEqual && typeIsEqual && weightIsEqual;
+}
+
+- (NSUInteger)hash {
+    return self.price.hash ^ self.type.hash ^ self.weight;
+}
+
 @end
